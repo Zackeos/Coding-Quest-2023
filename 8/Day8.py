f5f6bda87fcdc77f026413c9fdb582481d379273
Zackeos <zackfsummer@gmail.com>
2023-03-20 10:37:12 +0000

    not

--- 8/Day8.py
+++ 8/Day8.py
@@ -1,2 +1,27 @@
 with open("C:/Users/zacks/Documents/GitHub/Coding-Quest-2023/8/Day8.txt") as f:
-    data = f.read()
+    lines = f.readlines()
+
+
+
+# parse input data into adjacency matrix
+n = len(lines)
+graph = [[0] * n for _ in range(n)]
+for i in range(n):
+    row = list(map(int, lines[i].strip().split()))
+    for j in range(n):
+        graph[i][j] = row[j]
+
+# run nearest neighbor algorithm
+start = 0
+visited = set([start])
+current = start
+distance = 0
+while len(visited) < n:
+    nearest = min((graph[current][j], j) for j in range(n) if j not in visited)
+    distance += nearest[0]
+    current = nearest[1]
+    visited.add(current)
+distance += graph[current][start]
+
+# output result
+print(distance)
